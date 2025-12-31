fd87ed95a9effd0146b05f11e96451c3fbe6a5d9
Azharul Islam <azharulip2@gmail.com>
2025-12-31 10:53:14 +0600

    type error fixed

--- src/components/modules/dashboard/admin/help-and-support/HelpAndSupport.tsx
+++ src/components/modules/dashboard/admin/help-and-support/HelpAndSupport.tsx
@@ -343,14 +343,14 @@ export default function HelpAndSupport() {
                 stroke="#374151"
                 strokeWidth="1.25"
                 strokeLinecap="round"
-                stroke-linejoin="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667Z"
                 stroke="#374151"
                 strokeWidth="1.25"
                 strokeLinecap="round"
-                stroke-linejoin="round"
+                strokeLinejoin="round"
               />
             </svg>
           </div>
